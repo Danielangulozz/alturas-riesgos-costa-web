77289f5860fd751c3cc96fe96b460bcaf6239f37
Daniel Angulo <flooteo26@gmail.com>
2025-12-31 00:29:28 -0500

    supa
    asd

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://scqsqofwyaglaqtedomj.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_37IQIhRzzw2Y1UDvREJQ0g_mYoA5Oa4';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Creamos el cliente de forma segura
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
